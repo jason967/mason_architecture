589aefddb02a50945b050e01ec6e66302e71e491
최재웅 <jason967@naver.com>
2023-09-14 16:35:37 +0900

    sdddddd

--- hooks/pre_gen.dart
+++ hooks/pre_gen.dart
@@ -15,9 +15,9 @@ Future<void> run(HookContext context) async {
 }
 
 Future<void> _generateApp(HookContext context) async {
-  final progress = context.logger.progress('Running flutter project create');
+  final progress = context.logger.progress('Running flutter project create ');
 
   await Process.run('flutter', ['create', '{{name}}']);
-
+  await Process.run('flutter', ['pub', 'get']);
   progress.complete();
 }
